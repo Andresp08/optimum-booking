18857a1f24fac6800a208fe435ecd38f93287fd9
Andrés Pardo <Andresfp29032001@gmail.com>
2024-05-21 13:59:13 -0500

    Add dependencies for project start

---
+++ src/main/java/com/andresoft/optimumbooking/OptimumBookingApplication.java
@@ -0,0 +1,11 @@
+package com.andresoft.optimumbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimumBookingApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OptimumBookingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/andresoft/optimumbooking/OptimumBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresoft.optimumbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimumBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
